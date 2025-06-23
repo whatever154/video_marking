7785c26cf94689e87621173656260e4c655f86a2
whatever154 <159515725+whatever154@users.noreply.github.com>
2025-06-23 10:12:15 +0300

    Update video.py

--- video.py
+++ video.py
@@ -1,6 +1,8 @@
 import cv2
 from bs4 import BeautifulSoup
 import os
+import colordict
+import random
 
 
 def get_positions(xml):
@@ -8,12 +10,16 @@ def get_positions(xml):
         file_data = f.read()
     frames = dict()
     Bs_data = BeautifulSoup(file_data, "xml")
-    for i in Bs_data.find_all("data:bbox"):
-        framespan = i.get("framespan").split(":")
-        for j in range(int(framespan[0]), int(framespan[1]) + 1):
-            if j not in frames:
-                frames[j] = []
-            frames[j].append({"height": int(i.get("height")), "width": int(i.get("width")), "x": int(i.get("x")), "y": int(i.get("y"))})
+    Bs_data = Bs_data.find_all("object")
+    colors = list(colordict.ColorDict(mode="rgb").values())
+    for k in Bs_data:
+        obj_coolor = colors.pop(random.randint(0, len(colors) - 1))[-2::-1]
+        for i in k.find_all("data:bbox"):
+            framespan = i.get("framespan").split(":")
+            for j in range(int(framespan[0]), int(framespan[1]) + 1):
+                if j not in frames:
+                    frames[j] = []
+                frames[j].append({"color": obj_coolor,"height": int(i.get("height")), "width": int(i.get("width")), "x": int(i.get("x")), "y": int(i.get("y"))})
     return frames
 
 def video_edit(vid, change_frames, new_vid):
@@ -26,7 +32,7 @@ def video_edit(vid, change_frames, new_vid):
             if count in change_frames:
                 overlay = frame.copy()
                 for i in change_frames[count]:
-                    cv2.rectangle(overlay, (i["x"], i["y"]), (i["x"] + i["width"], i["y"] + i["height"]),  (0, 200, 0), -1)
+                    cv2.rectangle(overlay, (i["x"], i["y"]), (i["x"] + i["width"], i["y"] + i["height"]),  tuple(int(x) for x in i["color"]), -1)
                 alpha = 0.4
                 new_frame = cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0)
                 for i in change_frames[count]:
